f146d4cde0afd46a1d1947c3a0d4b08f6e04f22e
sultanaljamaie-collab <sultan_aljamaie@hotmail.com>
2026-03-05 16:53:31 +0300

    Update achievements.py

--- achievements.py
+++ achievements.py
@@ -1,1 +1,15 @@
+def get_medal(score):
 
+    if score >= 500:
+        return "🏆 عالم كبير"
+
+    if score >= 200:
+        return "🥇 عالم"
+
+    if score >= 100:
+        return "🥈 طالب علم"
+
+    if score >= 50:
+        return "🥉 مبتدئ"
+
+    return "📘 متعلم"
