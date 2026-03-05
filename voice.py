05734b40409158d642b52982df0f396917aa60da
sultanaljamaie-collab <sultan_aljamaie@hotmail.com>
2026-03-05 17:55:11 +0300

    Update voice.py

--- voice.py
+++ voice.py
@@ -2,9 +2,21 @@ from gtts import gTTS
 
 def speak(text):
 
+    formatted_text = f"""
+أهلاً بك في مسابقة طريق الجنة.
+
+{text}
+
+اختر الإجابة الصحيحة.
+"""
+
     filename = "voice.mp3"
 
-    tts = gTTS(text=text, lang="ar")
+    tts = gTTS(
+        text=formatted_text,
+        lang="ar",
+        slow=False
+    )
 
     tts.save(filename)
 
